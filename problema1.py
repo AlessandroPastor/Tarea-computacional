2672597bb2ecd63713f08decf71e56291f48cce4
AlessandroPastor <alexkip159alex@gmail.com>
2022-04-21 17:11:31 -0500

    first commit

---
+++ problema1.py
@@ -0,0 +1,18 @@
+#Escriba un algoritmo que lea 2 valores _a_, _b_, y los intercambie, por ejemplo si $a=2$ y $b=5$, el algoritmo debe hacer que $a=5$ y $b=2$
+
+a: int
+b: int
+t: int
+
+a = input("Ingrese el valor de a: ")
+b = input("Ingrese el valor de b: ")
+
+t = a
+a = b
+b = t
+
+print("el valor de a: ", a)
+print("el valor de b: ", b)
+print("el valor de t: ", t)
+
+
